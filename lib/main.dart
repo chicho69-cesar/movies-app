0b0c4f1cc3f07dfbca7711ae40596a30aa7094a8
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-09-08 21:04:31 -0500

    Models for movies and response added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/providers/movies_provider.dart';
+
+import 'package:provider/provider.dart';
 
 import 'package:movies_app/screens/screens.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MoviesProvider(),
+          /* La propiedad lazy esta por defecto en true, esta propiedad le indica
+          a flutter que la instancia de nuestro provider va a ser instanciada hasta que
+          un widget lo utilice, pero al ponerlo en false le estamos diciendo que tan 
+          pronto sea inicializada la aplicacion va a crear la instancia del provider */
+          lazy: false,
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,8 +38,8 @@ class MyApp extends StatelessWidget {
       title: 'Movies App',
       initialRoute: 'home',
       routes: {
-        'home': ( _ ) => const HomeScreen(),
-        'details': ( _ ) => const DetailsScreen(),
+        'home': (_) => const HomeScreen(),
+        'details': (_) => const DetailsScreen(),
       },
       theme: ThemeData.light().copyWith(
         appBarTheme: const AppBarTheme(
