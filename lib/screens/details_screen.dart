36314e2012b95014cb17b08e85193305884b8e6d
Cesar <cesarvillalobosolmos.01@gmail.com>
2022-09-08 21:03:29 -0500

    Finish details screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -37,7 +37,7 @@ class _CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-      backgroundColor: Colors.indigo,
+      // backgroundColor: Colors.indigo,
       expandedHeight: 200,
       floating: false,
       pinned: true,
